16ff8e245e53377b191efa8cc0016eb8719e0479
CK <chuck.jh.kim@gmail.com>
2026-06-11 16:25:38 +0000

    improved bgp_check

--- tests/test_bgp_check.py
+++ tests/test_bgp_check.py
@@ -1,47 +1,51 @@
 from logic.bgp_check import are_bgp_neighbors_established
 import pytest
 
+
 @pytest.mark.parametrize(
     "parsed_output, expected",
     [
-        (
+        pytest.param(
             {
                 "neighbor": {
                     "10.1.1.1": {"state": "Established"},
                     "10.1.1.2": {"state": "Established"},
-                    "10.1.1.3": {"state": "Idle"}
-                    },
+                    "10.1.1.3": {"state": "Idle"},
+                },
             },
-                False
-            ),
-        (
+            False,
+            id="idle_neighbor",
+        ),
+        pytest.param(
             {
                 "neighbor": {
                     "10.1.1.1": {"state": "Established"},
                     "10.1.1.2": {"state": "Established"},
-                    "10.1.1.3": {"state": "Established"}
-                    },
+                    "10.1.1.3": {"state": "Established"},
+                },
             },
-                True
-            ), 
-        (
+            True,
+            id="all_established",
+        ),
+        pytest.param(
             {
                 "neighbor": {
                     "10.1.1.1": {"state": "Established"},
                     "10.1.1.2": {"state": "Established"},
-                    "10.1.1.3": {"state": "Active"}
-                    },
+                    "10.1.1.3": {"state": "Active"},
+                },
             },
-                False
-            ),
-        (
+            False,
+            id="active_neighbor",
+        ),
+        pytest.param(
             {
-                    "neighbor": {}
+                "neighbor": {}
             },
-                False
-            ),
+            False,
+            id="no_neighbors",
+        ),
     ]
 )
-
 def test_are_bgp_neighbors_established(parsed_output, expected):
     assert are_bgp_neighbors_established(parsed_output) == expected
